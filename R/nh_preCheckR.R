6b3b528cf17e4784753012c9a18895e4846f6c9f
bwringe <bwringe@mun.ca>
2017-04-27 10:02:55 -0300

    added nh to match rest of functions

--- R/nh_preCheckR.R
+++ R/nh_preCheckR.R
@@ -1,13 +1,13 @@
-#' @name preCheckR
+#' @name nh_preCheckR
 #' @title Annotate Classes
 #'
-#' @description \code{preCheckR} detects cases where the NewHybrids MCMC chains fail to converge, and warns the user that the identified analyses should be repeated. When NewHybrids fails to converge, it is generally visible in cumulative probability plots (see nh_multiplotR) and identifiable in the PofZ file as an excess of F2 posterior probability. This function checks that the proportion of known Pure Population 1 or Pure Population 2 individuals assigned at least a
+#' @description \code{nh_preCheckR} detects cases where the NewHybrids MCMC chains fail to converge, and warns the user that the identified analyses should be repeated. When NewHybrids fails to converge, it is generally visible in cumulative probability plots (see nh_multiplotR) and identifiable in the PofZ file as an excess of F2 posterior probability. This function checks that the proportion of known Pure Population 1 or Pure Population 2 individuals assigned at least a
 #' @param PreDir the directory in which the NewHybrids results (in individual folders as returned by parallelNH_XX) are located.
 #' @param propCutOff The proportion of individuals in either Pure Population 1 OR Pure Population 2 allowed to exceed the F2 assignment threshold (PofZCutOff). The default is 0.5.
 #' @param PofZCutOff The threshold posterior probability of assignment (PofZ) to F2 above which Pure Population 1 or Pure Population 2 individuals are flagged to indicate possible non-convergence. The default is 0.1.
 #' @export
 
-preCheckR <- function(PreDir, propCutOff = 0.5, PofZCutOff=0.1){
+nh_preCheckR <- function(PreDir, propCutOff = 0.5, PofZCutOff=0.1){
 
   if(propCutOff >= 1 | propCutOff < 0){
     stop("propCutOff must be a proportion between 0 and 1. Please refer to help file")
