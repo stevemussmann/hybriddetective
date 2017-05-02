ed199ced303bee13d7691ee8abb4906d22233b4b
bwringe <bwringe@mun.ca>
2017-05-02 11:28:17 -0300

    Updated Zcore

--- R/NH_Zcore.R
+++ R/NH_Zcore.R
@@ -13,37 +13,27 @@
 
 nh_Zcore <- function(GetstheZdir, multiapplyZvec=NULL, applyuniqueZvec=NULL){
 
-  ## this function will insert a column of known genotype categories <- Zvec into a NewHybrids formatted dataset
-      ## for information on the known genotype categories, refer to the NewHybrids documentation (e.g.. Pure1 = z0, Pure2 = z1, F1 = z2 etc. [numbering follows that of NewHybrids])
-    ## fucntion will match a vector of categories (Zvec) to individuals, then output a NH format file, with a name indicating that the category vector
-      ## the user must provide the Zvec(s) to the function in the form of a two column .csv file(s) <- NOTE Zvec file must have column names, and the first column must be named "Individual" and
-        ## contain the names of individuals to which categories must be given that match those in the first column of the NewHybrids dataset
-
-  ## This is done in a batch fashion to every NewHybrids format file within a working folder -
-      #### IF a single file of Zvecs is to be applied to each file, USE multiapplyZvec
-          ###  multiapplyZvec must be specified as a file path + file name (e.g. "~/HoldenUniversity/CoolEthansComputer/HairDolls/Zvexxx.csv")
-
-      #### IF each NewHybrids file is to be given a UNIQUE vector of Zvecs use applyuniqueZvec ---- NOTE <- to apply UNIQUE Zvecs, the Zvec files must all be placed in a single folder
-          ### separate from the NewHybrids files AND they MUST follow the file name convention "NHFileName_Zvec.csv" where NHFileName is the same name as the file to which the Zvec is
-          ## to be applied - consequently, the number of NewHybrids files = number of Zvec files
-              ### applyuniqueZvec must be spedifed as a file path to the folder in which the Zvec files live (e.g. "~/HarrisonUniversity/DEANGordonPritchard/LettersOfRecommendation/")
-
-  ## The vector of genotype categories NEED NOT be the same length as the NewHybrids file - thus, time can be saved
-
-
-
 
 ## make sure that both multiapplyZvec and applyuniqueZvec haven't both been called
 
   if((length(multiapplyZvec) >0 ) && length(applyuniqueZvec) > 0){
     stop("One or the other bud. You can have multiapplyZvec, or applyuniqueZvec. You aren't a two-year-old at their birthday, so don't get greedy")
-  }
+    } # End if statement
 
 
-  ## files to be converted should all be in a single folder
-  ## get a list of the files to be converted - then initiate a loop to change each in turn
+## files to be converted should all be in a single folder
+## get a list of the files to be converted - then initiate a loop to change each in turn
   NHdataGet <- list.files(GetstheZdir)
       ## assumes you have given files in standard NewHybrids format
+
+  ### check to see that there are no individual files in the folder
+
+  indiv.file.exists <- grep(pattern = "individuals.txt", x = NHdataGet)
+
+  if(indiv.file.exists > 0){
+    NHdataGet <- NHdataGet[-indiv.file.exists]
+      }
+
 
   ## check if applyuniqueZvec has been called - and if so, error check the shit out of it
   if(length(applyuniqueZvec > 0)){
@@ -79,7 +69,7 @@ nh_Zcore <- function(GetstheZdir, multiapplyZvec=NULL, applyuniqueZvec=NULL){
     ## in order for separate to work properly, it must be given the nubmer of columns to divide - this is the number of loci included in NH file
         ## number of loci is given in the first 5 rows, and can be deterimined using a regex
     ## get number of loci using regex
-    SNPrep <- stringr::str_extract(string = addbackin, pattern = "NumLoci [:digit:]{2,5}") ## this does not cleanly take the number, so some cleaning is required
+    SNPrep <- str_extract(string = addbackin, pattern = "NumLoci [:digit:]{2,5}") ## this does not cleanly take the number, so some cleaning is required
       SNPrep <- as.numeric(gsub(x = SNPrep, pattern = "NumLoci ", replacement = ""))
       SNPrep <- SNPrep[which(is.na(SNPrep)==FALSE)]
 
@@ -103,24 +93,24 @@ nh_Zcore <- function(GetstheZdir, multiapplyZvec=NULL, applyuniqueZvec=NULL){
 
   }
 
-  #Zscorevector<-read.csv("/Users/brendanwringe/Desktop/DFO Aquaculture Interaction/South West Rivers Analysis/Frequency Based Sim/West/Zvector.csv")
-  Zscorevector$Individual <- as.character(Zscorevector$Individual)
-  #Merge
-  NHfinal<- base::merge(y=Nhdata3, x=Zscorevector, by="Individual", all=TRUE)
-  NHfinal <- NHfinal[order(as.numeric(NHfinal$Individual)),]
+#Zscorevector<-read.csv("/Users/brendanwringe/Desktop/DFO Aquaculture Interaction/South West Rivers Analysis/Frequency Based Sim/West/Zvector.csv")
+Zscorevector$Individual <- as.character(Zscorevector$Individual)
+#Merge
+NHfinal<- merge(y=Nhdata3, x=Zscorevector, by="Individual", all=TRUE)
+NHfinal <- NHfinal[order(as.numeric(NHfinal$Individual)),]
 
-  #head(NHfinal)
+#head(NHfinal)
 
-  #Now make this an actual file readable by NewHybrids
-  Loci <- do.call(paste,c(NHfinal[,], sep=" "))
-  Loci<-gsub("NA"," ",x = Loci)
-  Loci2<-c(addbackin,Loci)
-
-
-    NHdataZed <- gsub(x = i, pattern = ".txt", replacement = "_Zed.txt")
+#Now make this an actual file readable by NewHybrids
+Loci <- do.call(paste,c(NHfinal[,], sep=" "))
+Loci<-gsub("NA"," ",x = Loci)
+Loci2<-c(addbackin,Loci)
 
 
-  write.table(Loci2, file = paste0(GetstheZdir, NHdataZed),col.names=FALSE,row.names=FALSE,quote=FALSE)
+  NHdataZed <- gsub(x = i, pattern = ".txt", replacement = "_Zed.txt")
+
+
+write.table(Loci2, file = paste0(GetstheZdir, NHdataZed),col.names=FALSE,row.names=FALSE,quote=FALSE)
 
 
   } ## End of loop
